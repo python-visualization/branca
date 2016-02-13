a27055b3a9bd823a917a9a9847efac8dbd590238
Martin Journois <martin.journois@fr.michelin-solutions.com>
2016-02-13 15:27:28 +0100

    Add element in __init__

--- branca/__init__.py
+++ branca/__init__.py
@@ -3,8 +3,10 @@
 from __future__ import absolute_import
 
 import branca.colormap as colormap
+import branca.element as element
 
 __version__ = '0.0.0.dev'
 
 __all__ = ['colormap',
+           'element',
            ]
