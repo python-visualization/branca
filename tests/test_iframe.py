551f5a9ec2ab2101fd8132768b68073946538505
demonchyClement <cdemonchy@starclay.fr>
2016-09-28 22:30:24 +0000

    Add test of IFrame element, creation and rendering

---
+++ tests/test_iframe.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+""""
+Folium Element Module class IFrame
+----------------------
+"""
+import branca.element as elem
+from selenium import webdriver
+import base64
+def test_create_empty_iframe():
+	iframe = elem.IFrame()
+	iframe.render()
+
+def test_create_iframe():
+	iframe = elem.IFrame(html="<p>test content<p>",width=60,height=45)
+	iframe.render()
+
+def test_rendering_utf8_iframe():
+	iframe = elem.IFrame(html="<p>Cerrahpaşa Tıp Fakültesi</p>")
+	driver = webdriver.PhantomJS()
+	driver.get("data:text/html," + iframe.render())
+	driver.switch_to.frame(0);
+	assert "Cerrahpaşa Tıp Fakültesi" in driver.page_source
