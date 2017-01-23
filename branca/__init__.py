bd6d4d60cbcc5499b1a1e8f1db3da9cea4080af1
ocefpaf <ocefpaf@gmail.com>
2017-01-23 18:17:17 -0300

    bump version

--- branca/__init__.py
+++ branca/__init__.py
@@ -5,8 +5,9 @@ from __future__ import absolute_import
 import branca.colormap as colormap
 import branca.element as element
 
-__version__ = 'master'
+__version__ = '0.2.0'
 
-__all__ = ['colormap',
-           'element',
-           ]
+__all__ = [
+    'colormap',
+    'element',
+    ]
