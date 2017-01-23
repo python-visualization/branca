bdacd425ca686de417658a3c9f5fed106c6ff418
demonchyClement <cdemonchy@starclay.fr>
2017-01-23 20:26:15 +0000

    change test_iframe to respect coding standards

--- tests/test_iframe.py
+++ tests/test_iframe.py
@@ -5,18 +5,20 @@ Folium Element Module class IFrame
 """
 import branca.element as elem
 from selenium import webdriver
-import base64
+
+
 def test_create_empty_iframe():
-	iframe = elem.IFrame()
-	iframe.render()
+    iframe = elem.IFrame()
+    iframe.render()
+
 
 def test_create_iframe():
-	iframe = elem.IFrame(html="<p>test content<p>",width=60,height=45)
-	iframe.render()
+    iframe = elem.IFrame(html="<p>test content<p>", width=60, height=45)
+    iframe.render()
 
 def test_rendering_utf8_iframe():
-	iframe = elem.IFrame(html=u"<p>Cerrahpaşa Tıp Fakültesi</p>")
-	driver = webdriver.PhantomJS()
-	driver.get("data:text/html," + iframe.render())
-	driver.switch_to.frame(0);
-	assert u"Cerrahpaşa Tıp Fakültesi" in driver.page_source
+    iframe = elem.IFrame(html=u"<p>Cerrahpaşa Tıp Fakültesi</p>")
+    driver = webdriver.PhantomJS()
+    driver.get("data:text/html," + iframe.render())
+    driver.switch_to.frame(0)
+    assert u"Cerrahpaşa Tıp Fakültesi" in driver.page_source
