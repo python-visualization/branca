6a63020c101d6fc620f7422e38be59f548836e47
Filipe Fernandes <ocefpaf@gmail.com>
2019-02-15 11:19:51 -0200

    py3k only

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-# -*- coding: utf-8 -*-
-
 import os
+from pathlib import Path
 
 from setuptools import setup
 
 import versioneer
 
-rootpath = os.path.abspath(os.path.dirname(__file__))
+rootpath = Path(__file__).parent.absolute()
 
 
 def read(*parts):
-    return open(os.path.join(rootpath, *parts), 'r').read()
+    return open(rootpath.joinpath(*parts), "r").read()
 
 
 def walk_subpkg(name):
     data_files = []
-    package_dir = 'branca'
+    package_dir = "branca"
     for parent, dirs, files in os.walk(os.path.join(package_dir, name)):
         # Remove package_dir from the path.
         sub_dir = os.sep.join(parent.split(os.sep)[1:])
@@ -24,46 +23,49 @@ def walk_subpkg(name):
     return data_files
 
 
-pkg_data = {'': ['*.js',
-                 'plugins/*.js',
-                 'plugins/*.html',
-                 'plugins/*.css',
-                 'plugins/*.tpl',
-                 'templates/*.html',
-                 'templates/*.js',
-                 'templates/*.txt']}
-pkgs = ['branca', ]
+pkg_data = {
+    "": [
+        "*.js",
+        "plugins/*.js",
+        "plugins/*.html",
+        "plugins/*.css",
+        "plugins/*.tpl",
+        "templates/*.html",
+        "templates/*.js",
+        "templates/*.txt",
+    ]
+}
+pkgs = ["branca"]
 
-LICENSE = read('LICENSE.txt')
-long_description = '{}\n{}'.format(read('README.rst'), read('CHANGES.txt'))
+LICENSE = read("LICENSE.txt")
+long_description = "{}\n{}".format(read("README.rst"), read("CHANGES.txt"))
 
 # Dependencies.
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     tests_require = f.readlines()
 install_requires = [t.strip() for t in tests_require]
 
 
 setup(
-    name='branca',
+    name="branca",
     version=versioneer.get_version(),
-    description='Generate complex HTML+JS pages with Python',
+    description="Generate complex HTML+JS pages with Python",
     long_description=long_description,
-    author='Martin Journois',
-    url='https://github.com/python-visualization/branca',
-    keywords='data visualization',
+    author="Martin Journois",
+    url="https://github.com/python-visualization/branca",
+    keywords="data visualization",
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: MIT License',
-        'Development Status :: 5 - Production/Stable'
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
     ],
     packages=pkgs,
     package_data=pkg_data,
     include_package_data=True,
     cmdclass=versioneer.get_cmdclass(),
-    tests_require=['pytest'],
+    tests_require=["pytest"],
     license=LICENSE,
     install_requires=install_requires,
     zip_safe=False,
