c86a722b20d83e00c864dd574926b83539498b28
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-06-18 06:38:01 +0000

    Update docs after building Travis build 234 of python-visualization/branca
    The docs were built from the branch 'master' against the commit
    5887b9bce65be873c9c2ad867b3d6334b672b780.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/python-visualization/branca/jobs/699585911.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.8/site-packages/doctr/__main__.py deploy --sync --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1+2.g7a6c5ab',
+    VERSION: '0.4.1+3.g5887b9b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
