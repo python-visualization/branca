44f431fcc23f0c97c97ccb936aeb673ecdf99f44
Frank <33519926+Conengmo@users.noreply.github.com>
2021-04-18 15:34:46 +0200

    Add legend_scaler testcase

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,3 +1,5 @@
+import pytest
+
 import branca.utilities as ut
 
 
@@ -14,3 +16,14 @@ def test_color_brewer_reverse():
     scheme = ut.color_brewer('YlGnBu')
     scheme_r = ut.color_brewer('YlGnBu_r')
     assert scheme[::-1] == scheme_r
+
+
+@pytest.mark.parametrize("values,max_labels,expected", [
+    ([0, 1, 2, 3, 4], 10, [0, 1, 2, 3, 4]),
+    ([0, 1, 2, 3, 4], 2, [0, '', '', 3, '', '']),
+    ([3.2, 4.38, 5.56, 6.75, 7.93, 9.11, 10.3], 3, [3.2, '', '', 6.75, '', '', 10.3, '', '']),
+    ([0, 1, 2, 3, 4], 4, [0, '', 2, '', 4, '']),
+])
+def test_legend_scaler(values, max_labels, expected):
+    tick_labels = ut.legend_scaler(values, max_labels)
+    assert tick_labels == expected
