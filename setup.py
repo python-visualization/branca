5d4f915f6c4c8da2cdaac96ce902a6402fcde7de
Frank <33519926+Conengmo@users.noreply.github.com>
2024-04-04 21:36:11 +0200

    Drop Py3.7, add Py3.12

--- setup.py
+++ setup.py
@@ -53,11 +53,11 @@ setup(
     keywords="data visualization",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Development Status :: 5 - Production/Stable",
     ],
